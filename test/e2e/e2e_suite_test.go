532e3d7c285fbc6c2b1eb74f20c6e1eff932a545
Ken Moini <ken@kenmoini.com>
2026-05-01 22:30:06 -0400

    modify for e2e test endpoint

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -39,7 +39,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/jks-operator:v0.0.1"
+	projectImage = "example.com/jks-operator:e2e"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
