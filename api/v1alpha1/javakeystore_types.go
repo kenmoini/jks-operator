7e249e2db96405e9d70296e85f9223348e2a9782
Ken Moini <ken@kenmoini.com>
2026-05-01 23:12:00 -0400

    add jks api spec

--- api/v1alpha1/javakeystore_types.go
+++ api/v1alpha1/javakeystore_types.go
@@ -20,7 +20,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // JavaKeystoreSpec defines the desired state of JavaKeystore.
@@ -28,14 +27,54 @@ type JavaKeystoreSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of JavaKeystore. Edit javakeystore_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// RootCAConfigMaps is a list of ConfigMaps that contain Root CA Certificates.
+	RootCAConfigMaps []NamespacedConfigMapReference `json:"rootCAConfigMaps,omitempty"`
+
+	// AddDefaultCACertificates indicates whether to include the default CA certificates in the Java Keystore.  Defaults to false.
+	AddDefaultCACertificates bool `json:"addDefaultCACertificates,omitempty"`
+
+	// DefaultCACertificatesPath is the path to the default CA certificates file - in the UBI container it is at /etc/pki/ca-trust/extracted/pem/tls-ca-bundle.pem. This field is optionally and typically only used when the controller is running in a non-UBI container or externally.
+	DefaultCACertificatesPath string `json:"defaultCACertificatesPath,omitempty"`
+
+	// KeystorePasswordSecretRef is a reference to a Secret that contains the password for the Java Keystore. This field is optional and if omitted will use the default password "changeit".
+	KeyStorePasswordSecretRef NamespacedSecretReference `json:"keyStorePasswordSecretRef,omitempty"`
+
+	// TLSCertSecretRef is a reference to a list of Secrets that contain a TLS certificate and private key to be included in the Java Keystore. This field is optional and if omitted, no TLS certificate will be included in the Java Keystore.
+	TLSCertSecretRef []NamespacedSecretReference `json:"tlsCertSecretRef,omitempty"`
+}
+
+// ConfigMapReference defines a reference to a ConfigMap that contains a Certificate.
+// Must be in the same namespace as the JavaKeystore.
+type NamespacedConfigMapReference struct {
+	// Name of the ConfigMap.
+	Name string `json:"name"`
+
+	// Key in the ConfigMap that contains the Certificate.
+	// If left absent, all the keys in the ConfigMap will be used that match a PEM encoded certificate.
+	Key string `json:"key,omitempty"`
+}
+
+// SecretReference defines a reference to a Secret that contains the password for the Java Keystore or TLS Secret to include in the Java Keystore.
+// Must be in the same namespace as the JavaKeystore.
+type NamespacedSecretReference struct {
+	// Name of the Secret.
+	Name string `json:"name"`
+
+	// Key in the Secret that contains the password.
+	// If left absent, the key "password" will be used by default.
+	Key string `json:"key,omitempty"`
 }
 
 // JavaKeystoreStatus defines the observed state of JavaKeystore.
 type JavaKeystoreStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	// Conditions represents the latest available observations of the ClusterJavaKeystore's state.
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// Generated is a boolean that indicates whether the Java Keystore has been generated successfully.
+	Generated bool `json:"generated,omitempty"`
+
+	// ErrorMessage contains any error message related to the generation of the Java Keystore.
+	ErrorMessage string `json:"errorMessage,omitempty"`
 }
 
 // +kubebuilder:object:root=true
