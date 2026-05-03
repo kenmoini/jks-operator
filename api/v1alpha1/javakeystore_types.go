32a1d338fef04f40cceb09b356b465ddeb8bd33d
Ken Moini <ken@kenmoini.com>
2026-05-03 11:21:45 -0400

    whoops

--- api/v1alpha1/javakeystore_types.go
+++ api/v1alpha1/javakeystore_types.go
@@ -48,28 +48,6 @@ type JavaKeystoreSpec struct {
 	TargetSecret NamespacedSecretReference `json:"targetSecret,omitempty"`
 }
 
-// ConfigMapReference defines a reference to a ConfigMap that contains a Certificate.
-// Must be in the same namespace as the JavaKeystore.
-type NamespacedConfigMapReference struct {
-	// Name of the ConfigMap.
-	Name string `json:"name,omitempty"`
-
-	// Key in the ConfigMap that contains the Certificate.
-	// If left absent, all the keys in the ConfigMap will be used that match a PEM encoded certificate.
-	Key string `json:"key,omitempty"`
-}
-
-// SecretReference defines a reference to a Secret that contains the password for the Java Keystore or TLS Secret to include in the Java Keystore.
-// Must be in the same namespace as the JavaKeystore.
-type NamespacedSecretReference struct {
-	// Name of the Secret.
-	Name string `json:"name,omitempty"`
-
-	// Key in the Secret that contains the password.
-	// If left absent, the key "password" will be used by default.
-	Key string `json:"key,omitempty"`
-}
-
 // JavaKeystoreStatus defines the observed state of JavaKeystore.
 type JavaKeystoreStatus struct {
 	// Conditions represents the latest available observations of the JavaKeystore's state.
